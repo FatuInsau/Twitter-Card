fa0b9920066451ac6cdb0bcdba869de0eeb75058
FatuInsau <insaufaty@gmail.com>
2025-03-19 15:52:05 -0300

    renderizado de users

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
 import './App.css'
 import { TwitterFollorCard } from './TwitterFollowCard'
 
+const users = [
+    {
+        userName: 'midudev',
+        name: 'Miguel Ángel Durán',
+        firstFollowing: true,
+    },
+    {
+        userName: 'pheralb',
+        name: 'Pablo Hernandez',
+        firstFollowing: false,
+    },
+    {
+        userName: 'vxnder',
+        name: 'Elon Musk',
+        firstFollowing: false,
+    },
+    {
+        userName: 'TMChein',
+        name: 'Tomas',
+        firstFollowing: true
+    },
+]
+
 export function App () {
     return (
         <div className='App'>
-            <TwitterFollorCard userName="pheralb" name="Pablo Hernandez"/>
-            <TwitterFollorCard firstFollowing userName="midudev" name="Miguel Ángel Durán"/>
-            <TwitterFollorCard userName="vxnder" name="Elon Musk"/>
+            {
+                users.map(user => {
+                    const { userName, name, firstFollowing } = user;
+                    return (
+                        <TwitterFollorCard
+                        key={userName}
+                        userName={userName}
+                        name={name}
+                        firstFollowing={firstFollowing}/>
+                    )
+                })
+            }
         </div>
         
     )
