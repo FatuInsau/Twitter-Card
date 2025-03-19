605c6612ca86d8bdecf14406240210c10d0012a2
FatuInsau <insaufaty@gmail.com>
2025-03-19 15:39:39 -0300

    Stop Follow style

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export function App () {
     return (
         <div className='App'>
             <TwitterFollorCard userName="pheralb" name="Pablo Hernandez"/>
-            <TwitterFollorCard userName="midudev" name="Miguel Ángel Durán"/>
+            <TwitterFollorCard firstFollowing userName="midudev" name="Miguel Ángel Durán"/>
             <TwitterFollorCard userName="vxnder" name="Elon Musk"/>
         </div>
         
